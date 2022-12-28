b0ab0f09d96c378403d9cb2a656270b4a74a020f
DiogoBernardes <118618376+DiogoBernardes@users.noreply.github.com>
2022-12-28 01:01:15 +0000

    Remover Projeto (Check)

--- src/main/java/ttts/task_time_tracking_system/FreelancerRemoveProjectController.java
+++ src/main/java/ttts/task_time_tracking_system/FreelancerRemoveProjectController.java
@@ -10,9 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,21 +18,11 @@ import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class FreelancerEditProjectController implements Initializable{
-
+public class FreelancerRemoveProjectController implements Initializable {
     @FXML
     private ComboBox selectProject;
-    @FXML
-    private TextField nameProject;
-    @FXML
-    private TextField clientProject;
-    @FXML
-    private TextField priceHourProject;
-    @FXML
-    private ChoiceBox<ProjectState> stateProject;
 
     Projects actualProject;
-
     @FXML
     void backButton(ActionEvent event) {
         try {
@@ -42,26 +30,22 @@ public class FreelancerEditProjectController implements Initializable{
             Scene regCena = new Scene(root);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(regCena);
-            stage.setTitle("Menu Administrador");
+            stage.setTitle("Menu Freelancer");
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     @FXML
-    void editProject(ActionEvent event) throws IOException, ClassNotFoundException{
+    void removeProject(ActionEvent event) throws IOException, ClassNotFoundException{
         Map<Integer, Projects> projects = RepositoryProjects.deserialize("src\\main\\resources\\ttts\\Data\\Project.txt");
         for (Projects p : projects.values()) {
             if (p.getFreelancer().getNIF().equals(SessionData.freelancer.getNIF())) {
-                    ProjectState s = null;
-                    p.setName(nameProject.getText());
-                    p.setClient(clientProject.getText());
-                    p.setPriceHour(Float.parseFloat(priceHourProject.getText()));
-                    p.setState(stateProject.getValue());
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Sucesso");
-                    alert.setHeaderText("Dados Editados!");
-                    alert.show();
+                projects.remove(p.getIdProjeto());
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Sucesso");
+                alert.setHeaderText("Dados Removidos!");
+                alert.show();
             }
         }
         RepositoryProjects.getRepositoryProjects().serialize(projects,"src\\main\\resources\\ttts\\Data\\Project.txt");
@@ -92,10 +76,6 @@ public class FreelancerEditProjectController implements Initializable{
                 } catch (ClassNotFoundException cE) {
                     cE.getMessage();
                 }
-                nameProject.setText(actualProject.getName());
-                clientProject.setText(actualProject.getClient());
-                priceHourProject.setText(Float.toString(actualProject.getPriceHour()));
-                stateProject.setValue(actualProject.getState());
             }
         });
     }
--- src/main/java/ttts/task_time_tracking_system/ProjectsMenuController.java
+++ src/main/java/ttts/task_time_tracking_system/ProjectsMenuController.java
@@ -56,6 +56,19 @@ public class ProjectsMenuController implements Initializable {
             e.printStackTrace();
         }
     }
+    @FXML
+    void removeProject(ActionEvent event){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("removeProject.fxml"));
+            Scene regCena = new Scene (root);
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(regCena);
+            stage.setTitle("Listar Projetos");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     void backButton(ActionEvent event) {
